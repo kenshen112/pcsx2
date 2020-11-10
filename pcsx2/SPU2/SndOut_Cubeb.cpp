6ce0994db258a2194d9f04b686d9f902ed4d403f
kenshen112 <obarrtimothy@gmail.com>
2020-11-09 19:49:39 -0700

    Added proper error returns for SPU. Working on filling buffer, undefined refrences yay!

--- pcsx2/SPU2/SndOut_Cubeb.cpp
+++ pcsx2/SPU2/SndOut_Cubeb.cpp
@@ -3,6 +3,29 @@
 #include "Global.h"
 #include "SndOut.h"
 #include "cubeb/cubeb.h"
+
+long DataCallback(cubeb_stream * stm, void * user, const void * input_buffer, void * output_buffer, long nframes)
+{
+    const float * in  = (float*)input_buffer;
+    float * out = (float*)output_buffer;
+
+	for (u16 i = 0; i < nframes; i += SndOutPacketSize)
+	{
+		SndBuffer::ReadSamples(in);
+		for (int c = 0; c < 2; ++c) 
+		{
+            out[2 * i + c] = in[i];
+        }
+
+    }
+    return nframes;
+}
+
+void StateCallback(cubeb_stream * stm, void * user, cubeb_state state)
+{
+        printf("state=%d\n", state);
+}
+
 class SndCubeb : public SndOutModule
 {
 	//////////////////////////////////////////////////////////////////////////////////////////
@@ -25,26 +48,6 @@ class SndCubeb : public SndOutModule
 
     public:
 
-	static long DataCallback(cubeb_stream * stm, void * user, const void * input_buffer, void * output_buffer, long nframes)
-	{
-      const float * in  = (float*)input_buffer;
-      float * out = (float*)output_buffer;
-
-      for (int i = 0; i < nframes; ++i) 
-	  {
-          for (int c = 0; c < 2; ++c) 
-		  {
-            out[2 * i + c] = in[i];
-          }
-      }
-      return nframes;
-	}
-
-	static void StateCallback(cubeb_stream * stm, void * user, cubeb_state state)
-	{
-        printf("state=%d\n", state);
-	}
-
 	s32 Init() override
 	{
 		std::cout << "CUBEB AUDIO" << std::endl;
@@ -52,13 +55,14 @@ class SndCubeb : public SndOutModule
 		if (cubeb_init(&api, "PCSX2", NULL) != CUBEB_OK) // The inital startup of the underlying cubeb struct
 		{
 			DevCon.Error("INIT FAILED!");
+			return CUBEB_ERROR;
 		}
 		rv = cubeb_get_preferred_sample_rate(api, &rate);
 
 		if (rv != CUBEB_OK)
 		{
 			fprintf(stderr, "Could not get preferred sample-rate");
-			return rv;
+			return CUBEB_ERROR;
 		}
 
 		started = true; 
@@ -74,8 +78,10 @@ class SndCubeb : public SndOutModule
 		if(cubeb_stream_init(api, &stream, "PCSX2 Audio", nullptr, nullptr, nullptr, &outParams, latency_frames, DataCallback, StateCallback, this) != CUBEB_OK)
 		{
 			DevCon.Error("FAILED STREAM");
+			return CUBEB_ERROR;
 		}
 		cubeb_stream_start(stream);
+		return CUBEB_OK;
 	}
 
 	s32 Test() const override { return 0; }
