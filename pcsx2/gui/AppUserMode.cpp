5a96a123b70d5f1b920ed19ff4f6037986ac262f
Tyler Wilding <xtvaser@gmail.com>
2021-01-13 20:21:24 -0500

    debugging/temp - Hopefully resolve unicode issues around GameDB/portable.yaml loading

--- pcsx2/gui/AppUserMode.cpp
+++ pcsx2/gui/AppUserMode.cpp
@@ -64,8 +64,8 @@ InstallationModeType			InstallationMode;
 
 static fs::path GetPortableYamlPath()
 {
-	fs::path programDir = Path::GetExecutableDirectory();
-	return Path::Combine( programDir, "portable.yaml" );
+	fs::path programDir = Path::GetExecutableDirectory() / "portable.yaml";
+	return programDir.make_preferred();
 }
 
 static wxString GetMsg_PortableModeRights()
@@ -212,13 +212,22 @@ void Pcsx2App::WipeUserModeSettings()
 	}
 }
 
+static std::ifstream getFileAsStream(const fs::path& file)
+{
+#ifdef _WIN32
+	return std::ifstream(file.wstring());
+#else
+	return std::ifstream(file.string());
+#endif
+}
+
 bool Pcsx2App::Load(fs::path fileName)
 {
 	if (fs::exists(fileName))
 	{
 		try
 		{
-			stream = YAML::LoadFile(fileName);
+			stream = YAML::Load(getFileAsStream(fileName));
 			std::ostringstream os;
 			os << stream;
 			data = os.str();
