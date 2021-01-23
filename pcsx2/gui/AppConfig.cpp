811485711149226e90433fa80b872113d4959404
Tyler Wilding <xtvaser@gmail.com>
2021-01-23 15:27:52 -0500

    gui: Fold the separate MemoryCardListPanel config into AppConfig and removed duplicate code. Fix wxConfigFile singleton initialization

--- pcsx2/gui/AppConfig.cpp
+++ pcsx2/gui/AppConfig.cpp
@@ -17,7 +17,6 @@
 #include "App.h"
 #include "AppConfig.h"
 #include "MainFrame.h"
-
 #include "MemoryCardFile.h"
 
 #include "Utilities/IniInterface.h"
@@ -26,6 +25,7 @@
 #include <wx/stdpaths.h>
 #include "DebugTools/Debug.h"
 #include <memory>
+#include "fmt/core.h"
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // PathDefs Namespace -- contains default values for various pcsx2 path names and locations.
@@ -634,6 +634,7 @@ void AppConfig::LoadSave( IniInterface& ini )
 	// Process various sub-components:
 	ProgLogBox		.LoadSave( ini, L"ProgramLog" );
 
+	MemoryCardListPanel.LoadSave(ini);
 	Folders			.LoadSave( ini );
 	BaseFilenames	.LoadSave( ini );
 	GSWindow		.LoadSave( ini );
@@ -668,6 +669,20 @@ void AppConfig::ConsoleLogOptions::LoadSave( IniInterface& ini, const wxChar* lo
 	IniEntry( DisplaySize );
 	IniEntry( FontSize );
 	IniEntry( Theme );
+}
+
+void AppConfig::MemoryCardListPanelOptions::LoadSave(IniInterface& ini)
+{
+	ScopedIniGroup path( ini, L"MemoryCardListPanel" );
+
+	// TODO - config - once we switch to YAML, we could just iterate through what's in the file
+	// instead of having to have everything hard-coded...
+	for (auto const& columnInfo : LIST_VIEW_DEFAULT_COLUMN_INFO)
+	{
+		const int defValue = columnInfo.width;
+		const std::string key = fmt::format("ColumnWidth_{}", columnInfo.key);
+		ini.Entry(key, columnWidths, defValue);
+	}
 }
 
 void AppConfig::FolderOptions::ApplyDefaults()
@@ -1112,7 +1127,7 @@ void AppConfig_OnChangedSettingsFolder( bool overwrite )
 	Path::CreateFolder(PathDefs::GetDocuments());
 	Path::CreateFolder(GetSettingsFolder());
 
-	const wxString iniFilename( GetUiSettingsFilename() );
+	const wxString iniFilename(Path::ToWxString(GetUiSettingsFilename()));
 
 	if( overwrite )
 	{
