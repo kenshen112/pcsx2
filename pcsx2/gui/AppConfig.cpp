a64c8c5dc219c95ea8ff48222334b8d3bc90f6fd
t o <finalfantasytimothy@gmail.com>
2021-03-16 13:16:15 -0700

    AppConfig.cpp IniInterface.cpp: Fix to relative path saving. Remove GetPath for IniInterface to do it's job

--- pcsx2/gui/AppConfig.cpp
+++ pcsx2/gui/AppConfig.cpp
@@ -764,8 +764,8 @@ AppConfig::FolderOptions::FolderOptions()
 	, Cheats		( PathDefs::GetCheats() )
 	, CheatsWS      ( PathDefs::GetCheatsWS() )
 
-	//, RunIso(PathDefs::GetDocuments()) // raw default is always the Documents folder.
-	//, RunELF(PathDefs::GetDocuments()) // raw default is always the Documents folder.
+	, RunIso(PathDefs::GetDocuments()) // raw default is always the Documents folder.
+	, RunELF(PathDefs::GetDocuments()) // raw default is always the Documents folder.
 	, RunDisc(PathDefs::GetDocuments())
 {
 	bitset = 0xffffffff;
@@ -794,29 +794,20 @@ void AppConfig::FolderOptions::LoadSave( IniInterface& ini )
 	 //  --> on load, these relative paths will be expanded relative to the exe folder.
 	bool rel = ( ini.IsLoading() || IsPortable() );
 
-	//Bios = Path::getPath(Bios, PathDefs::GetDocuments(), IsPortable());
-	Snapshots = Path::getPath(Snapshots, PathDefs::GetDocuments(), IsPortable());
-	Savestates = Path::getPath(Savestates, PathDefs::GetDocuments(), IsPortable());
-	MemoryCards = Path::getPath(MemoryCards, PathDefs::GetDocuments(), IsPortable());
-	Logs = Path::getPath(Logs, PathDefs::GetDocuments(), IsPortable());
-	Langs = Path::getPath(Langs, PathDefs::GetDocuments(), IsPortable());
-	Cheats = Path::getPath(Cheats, PathDefs::GetDocuments(), IsPortable());
-	CheatsWS = Path::getPath(CheatsWS, PathDefs::GetDocuments(), IsPortable());
+	IniEntryDirFile(Bios, Bios, PathDefs::AppRoot(), false);
+	IniEntryDirFile(Snapshots, Snapshots, PathDefs::AppRoot(), rel );
+	IniEntryDirFile( Savestates, Savestates, PathDefs::AppRoot(), rel );
+	IniEntryDirFile( MemoryCards, MemoryCards, PathDefs::AppRoot(), false );
+	IniEntryDirFile( Logs, Logs, PathDefs::AppRoot(), rel );
+	IniEntryDirFile( Langs, Langs, PathDefs::AppRoot(), rel );
+	IniEntryDirFile( Cheats, Cheats, PathDefs::AppRoot(), rel );
+	IniEntryDirFile( CheatsWS, CheatsWS, PathDefs::AppRoot(), rel );
 
-	IniEntryDirFile( Bios,  rel);
-	IniEntryDirFile( Snapshots,  rel );
-	IniEntryDirFile( Savestates,  rel );
-	IniEntryDirFile( MemoryCards,  rel );
-	IniEntryDirFile( Logs,  rel );
-	IniEntryDirFile( Langs,  rel );
-	IniEntryDirFile( Cheats, rel );
-	IniEntryDirFile( CheatsWS, rel );
+	ini.Entry( L"PluginsFolder", PluginsFolder, PathDefs::AppRoot(), Path::Combine(InstallFolder, PathDefs::Base::Plugins()), rel );
 
-	ini.Entry( L"PluginsFolder", PluginsFolder, Path::Combine(InstallFolder, PathDefs::Base::Plugins()), rel );
-
-	IniEntryDirFile( RunIso, rel );
-	IniEntryDirFile( RunELF, rel );
-	IniEntryDirFile( RunDisc, rel );
+	IniEntryDirFile( RunIso, RunIso, PathDefs::AppRoot(), rel );
+	IniEntryDirFile( RunELF, RunELF, PathDefs::AppRoot(), rel );
+	IniEntryDirFile( RunDisc, RunDisc, PathDefs::AppRoot(), rel );
 
 	if( ini.IsLoading() )
 	{
